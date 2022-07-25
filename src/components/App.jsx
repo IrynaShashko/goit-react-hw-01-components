845df5cd81bcd8e40faa2efb83b21c8b3fecdae0
IrynaShashko <irishkashashko@gmail.com>
2022-07-25 13:12:39 +0300

    add task

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,28 @@
+import { Container } from "./App.styled";
+import { Profile } from "./Profile/Profile";
+import { Statistics } from "./Statistics/Statistics";
+import { Friends } from "./Friends/Friends";
+import { Transaction } from "./Transaction/Transaction";
+import user from '../data/user.json';
+import data from '../data/data.json';
+import friends from '../data/friends.json';
+import transactions from '../data/transactions.json';
+
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <Container>
+      <Profile
+      username={user.username}
+      tag={user.tag}
+      location={user.location}
+      avatar={user.avatar}
+      stats={user.stats}
+      />
+      <Statistics stats={data} title='Upload stats' />
+      <Statistics stats={data} />
+      <Friends friends={friends} />
+      <Transaction transactions={transactions} />
+    </Container>
   );
 };
